9ce2bb684684e6f170f3098e65b63a8672542680
Wout <wout3003@gmail.com>
2024-03-02 21:30:14 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>test</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class HomeView extends Vue {}
+</script>
